330c24f6f5cdda0d488eb89a13dd8c5070e9fa14
marzouk Baig <mbaig2@u.rochester.edu>
2020-12-05 18:12:17 -0500

    initial commit'

---
+++ src/StopContagion.java
@@ -0,0 +1,17 @@
+import java.io.FileReader;
+import java.io.IOException;
+
+public class StopContagion {
+
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+    public static void readData(String fileName) throws IOException {
+
+        FileReader
+    }
+}
